9b95d3c2c08ff15dadb1bb5562f5ddfd51ca157e
surajy5065 <surajy865272@gmail.com>
2026-02-11 13:39:23 +0530

    loop

---
+++ JS/loop.js
@@ -0,0 +1,47 @@
+// //print number from 1 to 10//
+//   for (i=1; i<11;i++){
+
+
+//    console.log(i);}
+
+
+// //print even number between 1to 50//
+// console.log("these are the even numbers")
+// for (i=0; i<=50;i++)
+// {   if(i%2==0)
+//     console.log(i)
+// }
+
+//print odd number between 1to 50//
+// console.log("these are the even numbers")
+// for (i=1; i<=50;i++)
+//     if(i<11)
+// {  
+//     console.log(`5 X ${i}=${5*i}`);
+// }
+// //print sum of 1 to 100//
+// console.log(`\n`)
+
+
+
+// for( i=1;i<11;i++)
+// {  j=i*i
+//     console.log(j)
+// }
+
+// for(i=1;i<11;i++)
+//    if(i%3==0)
+//    { console.log(i)
+//    }
+
+
+
+// let num = process.argv[2]
+// fact = 1
+// for (i = 1; i <= num; i++) { fact = fact * i }
+// console.log(fact)
+
+
+
+num=process.argv[2]
+for()
---
+++ JS/object.js
@@ -0,0 +1,31 @@
+let array =[
+    1,
+    2,
+    true,
+    false,
+    {name: "shivam"},
+    ['suraj','sneha'],
+    'renee',
+];
+
+
+console.log(array[0]);
+console.log(array[1]);
+console.log(array[2]);
+console.log(array[3]);
+console.log(array[4]);
+console.log(array[5]);
+console.log(array[6]);
+console.log(array[7]);
+
+
+for (let i=0; i < array.length; i++){
+    console.log(
+       `\n\ this is the value: ${array[i]} \n\ \n\ this is index:${i}`,
+    );
+}
+
+
+
+
+
