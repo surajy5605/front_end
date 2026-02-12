ab4337589b17c0e60c2bd015c876feee7758a28c
surajy5065 <surajy865272@gmail.com>
2026-02-12 13:51:42 +0530

    elseif

--- JS/object.js
+++ JS/object.js
@@ -1,18 +1,38 @@
-let obj={
-   animal:'dog',
-   married:false,
-   age:44,
-   city:['pune','kolhapur','nashik'],
-   hobbie:{
-    virtual:'pubg',
-    outdoor :'cricket',
-    myName: function(){
-        console.log(this.animal);
-},},
-};
-for (let key in object){
-    console.log('this is key ${key} this is value',object[key]);
+// let obj={
+//    animal:'dog',
+//    married:false,
+//    age:44,
+//    city:['pune','kolhapur','nashik'],
+//    hobbie:{
+//     virtual:'pubg',
+//     outdoor :'cricket',
+//     myName: function(){
+//         console.log(this.animal);
+// },},
+// };
+// for (let key in object){
+//     console.log('this is key ${key} this is value',object[key]);
+// }
+
+
+
+
+
+
+
+let num = +process.argv[2];
+
+if (num < 0) {
+    console.log("negative");
 }
+else if (num == 0) {
+    console.log('zero is not positive nor negative');
+}
+          
+else {
+    console.log("positive");
+}
+
 
 
 
