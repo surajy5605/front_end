600a7f890fc1dd2411a4c21dcb56555ade727b8f
surajy5065 <surajy865272@gmail.com>
2026-02-13 14:46:54 +0530

    updatelse

--- JS/object.js
+++ JS/object.js
@@ -44,3 +44,33 @@ else {
 
 
 
+
+
+
+
+
+ let n =+process.argv[3];
+
+if(n%2==0){
+    console.log("even number");
+}
+else{
+    console.log("odd number")
+}
+
+
+
+
+let num1 = 10;
+let num2 = 20;
+
+if (num1 > num2) {
+    console.log(num1 + " is greater");
+} else if (num2 > num1) {
+    console.log(num2 + " is greater");
+} else {
+    console.log("Both numbers are equal");
+}
+
+
+
