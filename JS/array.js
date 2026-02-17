e4e766fa5aaa95b456a8af84957e5441a9fdf7ad
surajy5065 <surajy865272@gmail.com>
2026-02-17 12:57:48 +0530

    methods

--- JS/array.js
+++ JS/array.js
@@ -13,10 +13,22 @@
 
 
 
-let arr=[]
-for(i=1;i<501;i++){
+// let arr=[]
+// for(i=1;i<501;i++){
    
-    arr.push(i)}
+//     arr.push(i)}
 
-const filterVal=arr.filter((el)=>el>50 && el<252 && el%2==0 )
-console.log(filterVal);
+// const filterVal=arr.filter((el)=>el>50 && el<252 && el%2==0 )
+// console.log(filterVal);
+
+
+
+let arr1=[1,2,3,4,5,6,7,8,9]
+
+const ans44=arr1.forEach((el)=>{
+    if (el % 2 ===0){
+        console.log(`this is even number ${el}`) 
+    } else{
+        console.log(`this is odd number ${el}`)
+    }
+})
