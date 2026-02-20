d78e97a9e9084c3fdad3c39d3582d6939f20d466
surajy5065 <surajy865272@gmail.com>
2026-02-20 12:38:05 +0530

    prototype

---
+++ JS/prototype.js
@@ -0,0 +1,23 @@
+let array = [1,2,3,4,4];
+let string ='hello';
+let object = {
+    name:'hello',
+};
+
+console.log(array);
+console.log(string);
+console.log(object);
+
+let ans = array.find((el)=> el ===3);
+console.log('ans');
+
+
+array.prototype.nikalo = function(){
+let index = this.length -1;
+
+delete this [index];
+}
+
+
+
+
