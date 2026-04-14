59bb5aad95ee399b6f5c88d8dc20ab355641a9b1
surajy5065 <surajy865272@gmail.com>
2026-04-14 13:05:01 +0530

    login

--- JS/gender/index.js
+++ JS/gender/index.js
@@ -1,28 +1,175 @@
-const radioValue = document.querySelector("[name='content']");
-console.log(radioValue)
+export const All_style = () => {
+  return ` * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        list-style: none;
+        text-decoration: none;
+        text-transform: capitalize;
+        font-weight: 600;
+      }
+      h1 {
+        text-align: center;
+        margin: 1rem 0;
+      }
+      nav {
+        display: flex;
+        justify-content: space-evenly;
+        align-items: center;
+        gap: 1.5rem;
+        box-shadow: 10px 10px 40px 2px gray;
+        padding: 20px;
+      }
+
+      ul {
+        display: contents;
+      }
+      
+      form{
+      display:flex;
+      justify-content:space-between;
+      align-items:center;
+      gap:1.5rem; 
+      width:60% ;   
+      margin:auto;
+      flex-wrap:wrap
+      padding:3rem 0
+      }
+
+    form>div:first-child{
+      display:flex;
+      flex-direction:column
+      }   
+      
+      div[name="btn"]{
+      display:flex;
+      justify-content:center;
+      padding:1.5rem 0px
+      }
+
+      div[name="btn"]>input, fieldset{
+      padding:10px 20px
+      }     
+
+      .message{
+      color:red;
+      font-size:0.9rem;
+      margin-top:5px;
+      }
+
+      header{
+      cursor:pointer;
+      }
+      `;
+};
+
+export const Navbar = () => {
+  return ` <nav>
+      <ul>
+        <li id="signupNav">signup</li>
+        <li id="loginNav">login</li>
+        <li id="adminNav">all_user</li>
+      </ul>
+    </nav>
+    `;
+};
+
+export const signup = () => {
+  return `
+    <form id="signupForm">
+        <div>
+            <div>
+                <label for="name">name</label>
+                <input id="name" type="text">
+                <span class="message"></span>
+            </div>
+            <br>           
+            
+            <div>
+                <label for="email">email</label>
+                <input id="email" type="text">
+                <span class="message"></span>
+            </div>
+            <br>
+
+            <div>
+                <label for="phone">phone</label>
+                <input id="phone" type="text">
+                <span class="message"></span>
+            </div>
+
+        </div>
+
+            <div>
+            <br>
+                <div>
+                    <label for="pass">password</label>
+                    <input id="pass" type="text">
+                    <span class="message"></span>
+                </div>
+            <br>
 
 
-const val=document.getElementsByTagName("input");
-console.log(val)
+                <div>
+                    <label for="confirm_pass">confirm password</label>
+                    <input id="confirm_pass" type="text">
+                    <span class="message"></span>
+                </div>
+            <br>
 
-let inputs = document.getElementsByName("content");
-console.log(inputs[0].value);
 
+                <div>
+                    <fieldset>
+                    <legend>gender</legend>
+                    <label for="male">male</label>
+                    <input id="male" value="male" name="gender" type="radio" />
 
+                    <label for="female">female</label>
+                    <input id="female" value="female" name="gender" type="radio" />
 
+                    <label for="other">other</label>
+                    <input id="other" value="other" name="gender" type="radio" />
+                    </fieldset>
+                </div>
 
-const handleSubmit = (e)=>{
-    e.preventDefault();
-    console.log('hello');
-    const radioVal= document.myRadio.content;
-     for (
-        let i=0; i<radioVal.length; i++
-     ){
-        if(radioVal[i].checked=== true){
-            document.querySelector('#output').innerHTML=`You are ${radioVal[i].value}`;
-            return true;
-        }
-     }
-     document.querySelector("#output").innerHTML="Please Select ur gender";
-     return false;
+            </div>
+
+            <input type="submit" value="button">
+    </form>
+   
+    
+    `;
+};
+
+export const login = () => {
+  return ` <form id="loginForm">
+            <div>         
+                    <div>
+                        <label for="email">email</label>
+                        <input id="email" type="text">
+                        <span class="message"></span>
+                    </div>
+                <br>
+                    <div>
+                        <label for="pass">password</label>
+                        <input id="pass" type="text">
+                        <span class="message"></span>
+                    </div>
+                <br>                
+            </div>
+                <input type="submit" value="login">
+            </form>
+    
+    `;
+};
+
+export const admin = () => {
+  if (data.email === 'admin' && data.pass === 'admin123') {
+  }
+  //
+  /* 
+  192.158.1.1
+  user:admin
+  pass:admin123  
+  */
 };
